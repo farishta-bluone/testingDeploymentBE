b630133bdee30f370d676d538445bfa7dc766748
farishta-bluone <farishta.sharma@bluone.in>
2021-03-15 18:57:55 +0530

    pickling od & operator added

--- models/slittedCoil.js
+++ models/slittedCoil.js
@@ -21,25 +21,27 @@ module.exports = class SlittedCoil {
         )
     }
     static fetchAll(query) {
-        let whereQuery = "";
-        if (query.status) {
-            let tempStatus = (query.status).toString().split(",");
-            if(tempStatus.length > 0) {
-                for (let i = 0; i< tempStatus.length; i++) {
-                    whereQuery = `${whereQuery} s.status = "${tempStatus[i]}"`
-                    if(i != (tempStatus.length-1)) whereQuery = `${whereQuery} OR` 
-                } 
-            }
-            else whereQuery = `s.status = "${query.status}"`
-        }
-        
-        else whereQuery = 's.status = "in-queue"'
-        
+        let whereQuery = 's.is_avilable = true'
         if(query.slit_date) whereQuery = `${whereQuery} and c.slit_date >= "${query.slit_date}" and c.slit_date < "${query.slit_date} 23:59:59"` 
         if(query.pickling_date) whereQuery = `${whereQuery} and s.pickling_date >= "${query.pickling_date}" and s.pickling_date < "${query.pickling_date} 23:59:59"`
         if(query.thickness) whereQuery = `${whereQuery} and c.thickness  = ${query.thickness}` 
         if(query.pickled_thickness) whereQuery = `${whereQuery} and s.pickled_thickness  = ${query.pickled_thickness}` 
         if(query.slit_shift) whereQuery = `${whereQuery} and c.slit_shift = ${query.slit_shift}`
+
+        let statusQuery = ""
+        if (query.status) {
+            let tempStatus = (query.status).toString().split(",");
+            if(tempStatus.length > 0) {
+                for (let i = 0; i< tempStatus.length; i++) {
+                    statusQuery = `${statusQuery} s.status = "${tempStatus[i]}"`
+                    if(i != (tempStatus.length-1)) statusQuery = `${statusQuery} OR` 
+                } 
+            }
+            else statusQuery = `s.status = "${query.status}"`
+        }
+        if(whereQuery && statusQuery) whereQuery = `${whereQuery} AND ${statusQuery}`
+       
+        
         return db.execute(`SELECT s.*, c.brand_no, c.company, c.thickness, c.weight, c.width, c.formulated_weight, c.date, c.slit_shift, c.slit_date 
         FROM slittedCoils s
         LEFT JOIN coils c
@@ -65,6 +67,8 @@ module.exports = class SlittedCoil {
         if(data.pickled_weight) query = `${query} pickled_weight = ${data.pickled_weight},`
         if(data.pickled_thickness) query = `${query}  pickled_thickness = ${data.pickled_thickness},`
         if(data.pickling_date) query = `${query}  pickling_date = "${data.pickling_date}",`
+        if(data.pickling_od) query = `${query}  pickling_od = ${data.pickling_od},`
+        if(data.pickling_operator) query = `${query}  pickling_operator = "${data.pickling_operator}",`
         if(data.pickling_notes) query = `${query}  pickling_notes = "${data.pickling_notes}",`
         if(data.pickling_shift) query = `${query}  pickling_shift = ${data.pickling_shift},`
         if(data.status) query = `${query} status = "${data.status}"`
